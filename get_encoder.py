b5470cf8836908e7ba4982840cd1071ae090d6db
Ammar <ammarlam10@gmail.com>
2022-07-21 17:53:33 +0200

    comment

--- get_encoder.py
+++ get_encoder.py
@@ -1,8 +1,102 @@
+import os
+
+import pandas as pd
+import seaborn as sn
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+from IPython.core.display import display
+from pl_bolts.datamodules import CIFAR10DataModule
+from pl_bolts.transforms.dataset_normalizations import cifar10_normalization
+from pytorch_lightning import LightningModule, Trainer, seed_everything
+from pytorch_lightning.callbacks import LearningRateMonitor
+from pytorch_lightning.callbacks.progress import TQDMProgressBar
+from pytorch_lightning.loggers import CSVLogger
+from torch.optim.lr_scheduler import OneCycleLR
+from torch.optim.swa_utils import AveragedModel, update_bn
+from torchmetrics.functional import accuracy
+
+
 from pl_bolts.models.self_supervised import SimCLR
 
-# load resnet50 pretrained using SimCLR on imagenet
+
+def create_model():
+    model = torchvision.models.resnet18(pretrained=False, num_classes=10)
+    model.conv1 = nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False)
+    model.maxpool = nn.Identity()
+    return model
+
+
+
+
+class LitResnet(LightningModule):
+    def __init__(self, lr=0.05):
+        super().__init__()
+
+        self.save_hyperparameters()
+        self.model = create_model()
+
+    def forward(self, x):
+        out = self.model(x)
+        return F.log_softmax(out, dim=1)
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        loss = F.nll_loss(logits, y)
+        self.log("train_loss", loss)
+        return loss
+
+    def evaluate(self, batch, stage=None):
+        x, y = batch
+        logits = self(x)
+        loss = F.nll_loss(logits, y)
+        preds = torch.argmax(logits, dim=1)
+        acc = accuracy(preds, y)
+
+        if stage:
+            self.log(f"{stage}_loss", loss, prog_bar=True)
+            self.log(f"{stage}_acc", acc, prog_bar=True)
+
+    def validation_step(self, batch, batch_idx):
+        self.evaluate(batch, "val")
+
+    def test_step(self, batch, batch_idx):
+        self.evaluate(batch, "test")
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.SGD(
+            self.parameters(),
+            lr=self.hparams.lr,
+            momentum=0.9,
+            weight_decay=5e-4,
+        )
+        steps_per_epoch = 45000 // BATCH_SIZE
+        scheduler_dict = {
+            "scheduler": OneCycleLR(
+                optimizer,
+                0.1,
+                epochs=self.trainer.max_epochs,
+                steps_per_epoch=steps_per_epoch,
+            ),
+            "interval": "step",
+        }
+        return {"optimizer": optimizer, "lr_scheduler": scheduler_dict}
+
+
+
 weight_path = 'cifar10model.pt'
-simclr = SimCLR.load_from_checkpoint(weight_path, strict=False)
+
+model = LitResnet(lr=0.05)
+
+model.load_state_dict(torch.load(PATH))
+
+
+
+# load resnet50 pretrained using SimCLR on imagenet
+#weight_path = 'cifar10model.pt'
+#simclr = SimCLR.load_from_checkpoint(weight_path, strict=False)
 
 simclr_resnet50 = simclr.encoder
 #simclr_resnet50.eval()
