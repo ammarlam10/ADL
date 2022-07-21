0fe2589fbf4b1be2cff57c90bc7131de2a1fa809
Ammar <ammarlam10@gmail.com>
2022-07-21 15:13:54 +0200

    comment

--- simclrcifar10.py
+++ simclrcifar10.py
@@ -35,7 +35,7 @@ NUM_WORKERS = int(os.cpu_count() / 2)
 
 
 # define train transformer
-train_transforms = torchvision.transforms.Compose(
+'''train_transforms = torchvision.transforms.Compose(
     [
         torchvision.transforms.RandomCrop(32, padding=4),
         torchvision.transforms.RandomHorizontalFlip(),
@@ -52,7 +52,7 @@ test_transforms = torchvision.transforms.Compose(
         cifar10_normalization(),
     ]
 )
-
+'''
 #cifar10_dm = CIFAR10DataModule(
 #    data_dir=PATH_DATASETS,
 #    batch_size=BATCH_SIZE,
@@ -63,16 +63,16 @@ test_transforms = torchvision.transforms.Compose(
 #)
 
 
-# DATASET
-cifar10_data_train = torchvision.datasets.CIFAR10('../', transform=train_transforms, train=True,download=True)
+# DATASET transform=train_transforms,
+cifar10_data_train = torchvision.datasets.CIFAR10('../',  train=True,download=True)
 
 # train loader
 train_data_loader = torch.utils.data.DataLoader(cifar10_data_train,
                                           batch_size=512,
                                           shuffle=True,
                                           num_workers=4)
-
-cifar10_data_test = torchvision.datasets.CIFAR10('../', transform=test_transforms, train=False,download=True)
+ #transform=test_transforms
+cifar10_data_test = torchvision.datasets.CIFAR10('../', train=False,download=True)
 test_data_loader = torch.utils.data.DataLoader(cifar10_data_test,
                                           batch_size=512,
                                           shuffle=True,
