233dab7be09531f8b604c6fc8c9af45218e26cf8
Ammar <ammarlam10@gmail.com>
2022-07-22 00:12:27 +0200

    comment

--- get_encoder.py
+++ get_encoder.py
@@ -1,10 +1,37 @@
-from pl_bolts.models.self_supervised import SimCLR
+import torch
+from torchvision.datasets import CIFAR10
 
-load resnet50 pretrained using SimCLR on imagenet
-weight_path = 'cifar10model.pt'
-simclr = SimCLR.load_from_checkpoint(weight_path, strict=False)
+import flash
+from flash.core.data.utils import download_data
+from flash.image import ImageClassificationData, ImageEmbedder
 
-simclr_resnet50 = model.encoder
-#simclr_resnet50.eval()
+# 1. Download the data and prepare the datamodule
+datamodule = ImageClassificationData.from_datasets(
+    train_dataset=CIFAR10(".", download=True),
+    batch_size=8,
+)
 
-print(simclr_resnet50)
+# 2. Build the task
+embedder = ImageEmbedder(
+    backbone="resnet18",
+    training_strategy="barlow_twins",
+    head="barlow_twins_head",
+    pretraining_transform="barlow_twins_transform",
+    training_strategy_kwargs={"latent_embedding_dim": 128},
+    pretraining_transform_kwargs={"size_crops": [32]},
+)
+
+
+model = embedder.load_from_checkpoint("image_embedder_simclr.pt")
+
+
+# 3. Create the trainer and pre-train the encoder
+trainer = flash.Trainer(max_epochs=1, gpus=torch.cuda.device_count())
+#trainer.fit(embedder, datamodule=datamodule)
+
+# 4. Save the model!
+
+embeddings = trainer.predict(model, datamodule=datamodule)
+
+# list of embeddings for images sent to the predict function
+print(embeddings)
