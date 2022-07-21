0a31f380ef5c86f43de72b130a1fe50843d92620
Ammar <ammarlam10@gmail.com>
2022-07-21 15:50:09 +0200

    comment

--- simclrcifar10.py
+++ simclrcifar10.py
@@ -1,4 +1,4 @@
-from pl_bolts.models.self_supervised import SimCLR
+'''from pl_bolts.models.self_supervised import SimCLR
 from pl_bolts.models.self_supervised.simclr import SimCLREvalDataTransform, SimCLRTrainDataTransform
 
 
@@ -114,3 +114,35 @@ EPOCH = 10
 PATH = "./cifar10model.pt"
 
 torch.save({'model_state_dict': model.state_dict()}, PATH)
+
+'''
+
+
+
+import os
+
+import pandas as pd
+import seaborn as sn
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+from IPython.core.display import display
+from pl_bolts.datamodules import CIFAR10DataModule
+from pl_bolts.transforms.dataset_normalizations import cifar10_normalization
+from pytorch_lightning import LightningModule, Trainer, seed_everything
+from pytorch_lightning.callbacks import LearningRateMonitor
+from pytorch_lightning.callbacks.progress import TQDMProgressBar
+from pytorch_lightning.loggers import CSVLogger
+from torch.optim.lr_scheduler import OneCycleLR
+from torch.optim.swa_utils import AveragedModel, update_bn
+from torchmetrics.functional import accuracy
+
+seed_everything(7)
+
+PATH_DATASETS = "/workspace/DATA"
+BATCH_SIZE = 256 if torch.cuda.is_available() else 64
+NUM_WORKERS = int(os.cpu_count() / 2)
+
+print(NUM_WORKERS)
+print(PATH_DATASETS)
